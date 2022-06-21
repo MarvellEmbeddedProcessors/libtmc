1fcba6ae0ec60ea7dd47ddf8224c8e934479dc31
Pavan Nikhilesh <pbhagavatula@marvell.com>
2022-06-21 12:50:13 +0530

    isol: use timed pthread join
    Use timed pthread join to prevent master for waiting indefinately.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: Ied1c674c01d6e448886bc44237c108daf052fde4

--- isol.c
+++ isol.c
@@ -3053,10 +3053,12 @@ void memipc_master_handle_request(int read_req_type,
 				  unsigned char *memipc_read_buffer,
 				  struct memipc_thread_params *thread)
 {
+	static int thread_last_cpu = -1, last_newline = 1;
+	struct timespec ts;
+	int client_index;
 	char buffer[19];
 	char zero = 0;
-	static int thread_last_cpu = -1, last_newline = 1;
-	int client_index;
+	int s;
 
 	(void)zero;
 
@@ -3190,8 +3192,10 @@ void memipc_master_handle_request(int read_req_type,
 		if (thread->foreign_desc != NULL)
 			memipc_detach_thread_from_desc(thread->foreign_desc);
 		if (thread->pid == getpid()) {
+			clock_gettime(CLOCK_REALTIME, &ts);
+			ts.tv_sec += 5;
 			/* Thread from the same process, join it. */
-			pthread_join(thread->thread_id, NULL);
+			pthread_timedjoin_np(thread->thread_id, NULL, &ts);
 		}
 #if ISOLATION_MONITOR_IN_MASTER
 #ifdef DEBUG_LOG_ISOL_CHANGES
@@ -4825,8 +4829,12 @@ static int client_text_handler(int client_index, const char *line)
 				memipc_detach_thread_from_desc(
 						       thread->foreign_desc);
 			if (thread->pid == getpid()) {
+				struct timespec ts;
+
+				clock_gettime(CLOCK_REALTIME, &ts);
+				ts.tv_sec += 5;
 				/* Thread from the same process, join it. */
-				pthread_join(thread->thread_id, NULL);
+				pthread_timedjoin_np(thread->thread_id, NULL, &ts);
 			}
 #if ISOLATION_MONITOR_IN_MASTER
 #ifdef DEBUG_LOG_ISOL_CHANGES
@@ -4876,8 +4884,12 @@ static int client_disconnect_handler(int client_index)
 		if (thread->foreign_desc != NULL)
 			memipc_detach_thread_from_desc(thread->foreign_desc);
 		if (thread->pid == getpid()) {
+			struct timespec ts;
+
+			clock_gettime(CLOCK_REALTIME, &ts);
+			ts.tv_sec += 5;
 			/* Thread from the same process, join it. */
-			pthread_join(thread->thread_id, NULL);
+			pthread_timedjoin_np(thread->thread_id, NULL, &ts);
 		}
 #if ISOLATION_MONITOR_IN_MASTER
 #ifdef DEBUG_LOG_ISOL_CHANGES
